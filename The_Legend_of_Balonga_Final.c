66e4bb572cd9e7213e68b22be92adb87b7a91994
ThiagoMorango <thiago.morango.personal@gmail.com>
2020-04-19 14:39:58 +0900

    Working Combat && main
    Changelog:
    - Fixed typedef of struct Entity;
    - Fixed UpdateInfo with strcpy;
    - Added code documentation comments to all functions;
    - Replaced getch() with system("pause") when waiting for user input;
    - Refactored code to work accordingly to memory allocation;
    - Something else I might have forgotten.

--- The_Legend_of_Balonga_Final.c
+++ The_Legend_of_Balonga_Final.c
@@ -6,16 +6,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Entity{
+typedef struct Entity{
     char name[40];
     int attack;
     int defense;
     int healthPoints;
     int level;
     int weaponModifier;
-};
+} Entity;
 
-//TODO Test to see if this works as intended
 /**
  * UpdateInfo function
  *
@@ -24,7 +23,7 @@ struct Entity{
  *  Updates an Entity's structure members by replacing the old ones for the variables received as parameters.
  *  If not intending to replace all members, just use the original one as a parameter when calling the function.
  *
- *  @param EntityToBeUpdated - pointer of the original entity to have the values updated
+ *  @param EntityToBeUpdated - Pointer of the original entity to have the values updated
  *  @param newName - The variable to be allocated in the member of the original entity of the same name.
  *  @param newAttack - The variable to be allocated in the member of the original entity of the same name.
  *  @param newDefense - The variable to be allocated in the member of the original entity of the same name.
@@ -32,28 +31,54 @@ struct Entity{
  *  @param newLevel - The variable to be allocated in the member of the original entity of the same name.
  *  @param newWeaponModifier - The variable to be allocated in the member of the original entity of the same name.
  */
-void UpdateInfo(struct Entity* EntityToBeUpdated, char newName[40], int newAttack, int newDefense, int newHealthPoints, int newLevel, int newWeaponModifier){
-    EntityToBeUpdated->name = newName;
+void UpdateInfo(Entity* EntityToBeUpdated, char newName[40], int newAttack, int newDefense, int newHealthPoints, int newLevel, int newWeaponModifier){
+    strcpy(EntityToBeUpdated->name, newName);
     EntityToBeUpdated->attack = newAttack;
     EntityToBeUpdated->defense = newDefense;
     EntityToBeUpdated->healthPoints = newHealthPoints;
     EntityToBeUpdated->weaponModifier = newWeaponModifier;
 }
 
-void printInfo(struct Entity* EntityToBePrinted) {
-    printf("---------------------------------------------------------------------------------------\n");
+/**
+ *  PrintInfo function
+ *
+ *  @author NeTissues
+ *
+ *  Prints an Entity's Name, Health Points, Level, Attack and Defense in a formatted form.
+ *
+ *  @param EntityToBePrinted - Pointer of the entity to have it's parameters (not all) printed.
+ *
+ */
+void PrintInfo(Entity* EntityToBePrinted) {
+    printf("-------------------------------------------------------------------------------\n");
     printf("\tName: %s \t\t lvl: %d\n", EntityToBePrinted->name, EntityToBePrinted->level);
     printf("\tATK: %i \t\t DEF: %i\n", EntityToBePrinted->attack, EntityToBePrinted->defense);
-    printf("\tHP: %s\n", EntityToBePrinted->healthPoints);
-    printf("---------------------------------------------------------------------------------------\n");
+    printf("\tHP: %i\n", EntityToBePrinted->healthPoints);
+    printf("-------------------------------------------------------------------------------\n");
 }
 
-void onEnemyDeath() {
-
+/**
+ * OnEnemyDeath function
+ * 
+ * @author NeTissues
+ * 
+ * 
+ * 
+ */
+void OnEnemyDeath() {
+    printf("Enemy died\n");
 }
 
-void onDeath() {
-
+/**
+ * OnDeath function
+ * 
+ * @author NeTissues
+ * 
+ * 
+ * 
+ */
+void OnDeath() {
+    printf("Player died\n");
 }
 
 /**
@@ -77,19 +102,18 @@ int DamageDealtCalculation(int actorAttack, int weaponModifier, int enemyDefense
 
     //d20 is a simulation of a 20 sided dice roll, from 1 to 20
     d20 = rand() % 20 + 1;
-
     //Then, based on the result of the roll we can treat the attack as being one of the following:
     //Crit, when a 20 is rolled. Fail, When a number between 1 and 5 and a Normal attack with anything else
 
     //Crit
     if(d20 == 20){
-        damageDealt = (actorAttack + (weaponModifier/100)) * 2 - enemyDefense;
+        damageDealt = ((actorAttack + (weaponModifier/100)) * 2) - enemyDefense;
         //Fail
-    }else if(d20 <= 5){
+    }else if(d20 <= 2){
         damageDealt = 0;
         //Normal attack
     }else{
-        damageDealt = (actorAttack + weaponModifier/100) + rand() % 5 - enemyDefense;
+        damageDealt = ((actorAttack + weaponModifier/100) + rand() % 5) - enemyDefense;
     }
 
     return damageDealt;
@@ -98,20 +122,18 @@ int DamageDealtCalculation(int actorAttack, int weaponModifier, int enemyDefense
 /**
  * Combat function
  *
- *  @param enemyAttack
- *  @param enemyDefense
- *  @param enemyHP
- *  @param enemyWeaponModifier
+ *  @author NeTissues
  *
- *  @param playerAttack
- *  @param playerDefense
- *  @param playerHP
- *  @param playerWeaponModifier
+ *
+ *
+ *  @param Player -
+ *  @param Enemy -
+ *
  */
-void Combat(struct Entity* Player, struct Entity* Enemy){
+void Combat(Entity* Player, Entity* Enemy){
 
     //variable declarations
-    int combatOption, int damageToEnemy, int damageToPlayer;
+    int combatOption, damageToEnemy, damageToPlayer;
 
     do {
         //First, clear the screen so that the player can keep track of what's happening
@@ -124,12 +146,13 @@ void Combat(struct Entity* Player, struct Entity* Enemy){
         printf("-------------------------------------------------------------------------------\n");
         printf("\t\t\t\t    BATTLE\n");
 
+        //Print the info of the battle actors on the screen
+        PrintInfo(Player);
+        PrintInfo(Enemy);
+
         //Reading the player's choice
         scanf("%i", &combatOption);
 
-        //Print the info of the battle actors on the screen
-        printInfo(Player);
-        printInfo(Enemy);
 
         //Switch case
         switch (combatOption) {
@@ -144,18 +167,16 @@ void Combat(struct Entity* Player, struct Entity* Enemy){
                 printf("You have dealt %i damage to %s \n", damageToEnemy, Enemy->name);
                 printf("You have taken %i damage from %s \n", damageToPlayer, Enemy->name);
 
-                enemyHP = enemyHP - damageToEnemy;
-                playerHP = playerHP - damageToPlayer;
-
-                printf("Current player HP: %i \n", playerHP);
-                printf("Current enemy HP: %i \n", playerHP);
+                Enemy->healthPoints -= damageToEnemy;
+                Player->healthPoints -= damageToPlayer;
 
                 //Wait for a user input before clearing the screen
-                getch();
+                system("pause");
 
                 break;
 
                 //Defend
+                //TODO think about implementing power strikes
             case 2:
 
                 break;
@@ -163,47 +184,58 @@ void Combat(struct Entity* Player, struct Entity* Enemy){
             default:
                 break;
         }
-    }while(enemyHP>=0 || playerHP>=0);
-    if (enemyHP <= 0){
-        onEnemyDeath();
-    }else if (playerHP <= 0){
-        onDeath();
+    }while(Enemy->healthPoints>=0 && Player->healthPoints>=0);
+
+    if (Enemy->healthPoints <= 0){
+        OnEnemyDeath();
+    }else if (Player->healthPoints <= 0){
+        OnDeath();
     }
 }
 
 int main(){
 
     //Entities declaration
-    struct Entity Player;
-    struct Entity Dummy;
+    Entity* Player;
+    Player = malloc(sizeof(Entity));
+
+    Entity* Dummy;
+    Dummy = malloc(sizeof(Entity));
 
     //Variables declaration
     char nameVariable[40];
 
     printf("*Insert useless lore that no one reads here*\n");
-    getch(); //Wait for a user input
+    //Wait for a user input
+    system("pause");
     printf("Insert your character's name\n");
     gets(nameVariable);
 
-    //TODO re-do this
     //Entities specification
     /*
      * Player specification
     */
-    strcpy( Player.name, nameVariable);
-    Player.attack = 5;
-    Player.defense = 0;
-    Player.healthPoints = 100;
-    Player.weaponModifier = 0;
+    strcpy( Player->name, nameVariable);
+    Player->attack = 5;
+    Player->defense = 0;
+    Player->healthPoints = 100;
+    Player->level = 1;
+    Player->weaponModifier = 10;
     //-----------------------------------------
     /*
      * Dummy specification
     */
-    strcpy( Dummy.name, "Target Dummy");
-    Dummy.attack = 0;
-    Dummy.defense = 0;
-    Dummy.healthPoints = 100;
-    Dummy.weaponModifier = 0;
+    strcpy( Dummy->name, "Target Dummy");
+    Dummy->attack = 0;
+    Dummy->defense = 0;
+    Dummy->healthPoints = 100;
+    Dummy->level = 0;
+    Dummy->weaponModifier = 0;
+    //-----------------------------------------
 
     Combat(Player, Dummy);
+
+    //Freeing memory allocations after last use.
+    free(Player);
+    free(Dummy);
 }
