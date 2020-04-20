00b691f594c245e1811cd7dffa79c4df2e36c54b
ThiagoMorango <thiago.morango.personal@gmail.com>
2020-04-20 14:38:43 +0900

    Started experience system

--- The_Legend_of_Balonga_Final.c
+++ The_Legend_of_Balonga_Final.c
@@ -1,6 +1,7 @@
 //
 // Created by NeTissues on 2020-04-14.
 //
+//TODO Search: Is it better to separate the multiple combat options into separate functions or leave them as one?
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,10 +11,14 @@ typedef struct Entity{
     char name[40];
     int attack;
     int defense;
+    float experience; //Does it even make sense for a non playable Entity to have experience?
     int healthPoints;
     int level;
     int weaponModifier;
 } Entity;
+//TODO Think: how to go about creating a new struct for weapons for a more diverse combat && if it is even worth it.
+
+//TODO Create a character customisation with attributes.
 
 /**
  * UpdateInfo function
@@ -31,12 +36,30 @@ typedef struct Entity{
  *  @param newLevel - The variable to be allocated in the member of the original entity of the same name.
  *  @param newWeaponModifier - The variable to be allocated in the member of the original entity of the same name.
  */
-void UpdateInfo(Entity* EntityToBeUpdated, char newName[40], int newAttack, int newDefense, int newHealthPoints, int newLevel, int newWeaponModifier){
+void UpdateInfo(Entity* EntityToBeUpdated, char newName[40], int newAttack, int newDefense, float newExperience, int newHealthPoints, int newLevel, int newWeaponModifier){//TODO Delete this if kept unused.
     strcpy(EntityToBeUpdated->name, newName);
     EntityToBeUpdated->attack = newAttack;
     EntityToBeUpdated->defense = newDefense;
+    EntityToBeUpdated->experience = newExperience;
     EntityToBeUpdated->healthPoints = newHealthPoints;
+    EntityToBeUpdated->level = newLevel;
     EntityToBeUpdated->weaponModifier = newWeaponModifier;
+}
+
+void LevelUp(Entity* Player){
+    Player->attack += 1;
+    Player->defense +=1;
+
+}
+
+void ExperienceGain(Entity* Player){
+    //Add 1 to the Player experience
+    Player->experience += 1;
+    //Check if the experience has reached the goal needed for a level up
+    if(Player->experience >=3){
+        Player->level +=1;
+        LevelUp();
+    }
 }
 
 /**
@@ -45,6 +68,7 @@ void UpdateInfo(Entity* EntityToBeUpdated, char newName[40], int newAttack, int 
  *  @author NeTissues
  *
  *  Prints an Entity's Name, Health Points, Level, Attack and Defense in a formatted form.
+ *  Meant to be used on Combat()
  *
  *  @param EntityToBePrinted - Pointer of the entity to have it's parameters (not all) printed.
  *
@@ -59,25 +83,28 @@ void PrintInfo(Entity* EntityToBePrinted) {
 
 /**
  * OnEnemyDeath function
- * 
+ *
  * @author NeTissues
- * 
- * 
- * 
+ *
+ *
+ *
  */
-void OnEnemyDeath() {
+void OnEnemyDeath(Entity* Player) {
+    //TODO Create exp gain system.
     printf("Enemy died\n");
+    ExperienceGain(Player);
 }
 
 /**
  * OnDeath function
- * 
+ *
  * @author NeTissues
- * 
- * 
- * 
+ *
+ *
+ *
  */
 void OnDeath() {
+    //TODO Create a checkpoint system to resume the game at after player death.
     printf("Player died\n");
 }
 
@@ -117,7 +144,7 @@ int DamageDealtCalculation(int actorAttack, int weaponModifier, int enemyDefense
     }
 
     return damageDealt;
-}
+}//TODO Create a new function for making complex attack patterns for enemies and handling multiple combat options for them.
 
 /**
  * Combat function
@@ -130,7 +157,7 @@ int DamageDealtCalculation(int actorAttack, int weaponModifier, int enemyDefense
  *  @param Enemy -
  *
  */
-void Combat(Entity* Player, Entity* Enemy){
+void Combat(Entity* Player, Entity* Enemy){//TODO Finish this
 
     //variable declarations
     int combatOption, damageToEnemy, damageToPlayer;
@@ -176,9 +203,9 @@ void Combat(Entity* Player, Entity* Enemy){
                 break;
 
                 //Defend
-                //TODO think about implementing power strikes
+                //TODO think about implementing power strikes.
             case 2:
-
+                //TODO Finish implementing defense option.
                 break;
                 //Unknown input handler
             default:
@@ -187,7 +214,7 @@ void Combat(Entity* Player, Entity* Enemy){
     }while(Enemy->healthPoints>=0 && Player->healthPoints>=0);
 
     if (Enemy->healthPoints <= 0){
-        OnEnemyDeath();
+        OnEnemyDeath(Player);
     }else if (Player->healthPoints <= 0){
         OnDeath();
     }
@@ -218,6 +245,7 @@ int main(){
     strcpy( Player->name, nameVariable);
     Player->attack = 5;
     Player->defense = 0;
+    Player->experience = 0;
     Player->healthPoints = 100;
     Player->level = 1;
     Player->weaponModifier = 10;
@@ -228,13 +256,14 @@ int main(){
     strcpy( Dummy->name, "Target Dummy");
     Dummy->attack = 0;
     Dummy->defense = 0;
+    Dummy->experience = 0;
     Dummy->healthPoints = 100;
     Dummy->level = 0;
     Dummy->weaponModifier = 0;
     //-----------------------------------------
 
     Combat(Player, Dummy);
-
+    printf("%i", Player->healthPoints);
     //Freeing memory allocations after last use.
     free(Player);
     free(Dummy);
